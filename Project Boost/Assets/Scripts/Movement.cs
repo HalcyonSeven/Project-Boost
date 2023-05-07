7df5f409b690cdd1d00ca3902a142ccc771929a4
Tristan Devadason <67545352+HalcyonSeven@users.noreply.github.com>
2023-05-07 18:14:47 +0800

    update movement control and refactor rotate code

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody rocketRb;
+    [SerializeField] private float thrust =1000f;
+    [SerializeField] private float rotationSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rocketRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketRb.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+        }
+
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rocketRb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rocketRb.freezeRotation = false;
     }
 }
+
